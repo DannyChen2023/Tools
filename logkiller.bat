d4496df477b4ef56bf78ff9552dc153526181bf1
DannyChen <51437574+DannyChen2023@users.noreply.github.com>
2023-05-16 09:56:36 +0800

    Update logkiller.bat

--- logkiller.bat
+++ logkiller.bat
@@ -1,10 +1,15 @@
 @echo off
 color 7
 title Log Killer
-echo  -==========================================-
-echo -=       Corecloud Log killer v20230516     =-
-echo -=   This tool going to delete all logs !   =-
-echo  -==========================================-
+echo  -====================================================-
+echo -=    ____                ____ _                 _   =-
+echo -=   / ___|___  _ __ ___ / ___| | ___  _   _  __| |  =-
+echo -=  | |   / _ \| '__/ _ \ |   | |/ _ \| | | |/ _` |  =-
+echo -=  | |__| (_) | | |  __/ |___| | (_) | |_| | (_| |  =-
+echo -=   \____\___/|_|  \___|\____|_|\___/ \__,_|\__,_|  =-
+echo -=       Corecloud Log killer v20230516              =-
+echo -=   This tool going to delete all logs !            =-
+echo  -====================================================-
 timeout 5
 for /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :clear "%%G")
 echo.
